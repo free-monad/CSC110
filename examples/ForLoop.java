6b794375f36ef113e1778fe9ffe3cc2632aea365
free-monad <54972647+free-monad@users.noreply.github.com>
2019-09-28 19:52:13 -0400

    Create ForLoop.java

---
+++ examples/ForLoop.java
@@ -0,0 +1,9 @@
+public class ForLoop{
+  public static void main(String [] args){
+    int howManyTimes = 10;
+    
+    for(int i = 1; i <= 10; i++){
+      System.out.println("Running the loop " + i + " times!");
+    }
+  }
+}
