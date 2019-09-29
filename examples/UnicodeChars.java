033f44543101ee218c395e215ae2b4ad1a076222
Matthew Conroy <matthewscottconroy@gmail.com>
2019-09-29 10:50:51 -0400

    Added Unicode example program

---
+++ examples/UnicodeChars.java
@@ -0,0 +1,30 @@
+public class UnicodeChars {
+	public static void main(String [] args){
+
+		final char SMILEY_FACE = '\u263A';
+		final char HEART = '\u2764';
+		final char SEVEN = '\u0037';
+		final char THREE = '\u0033';
+		final char FROWN = '\u2639';
+		final char DIAMOND = '\u2666';
+		final char EX = '\u2743';
+		final char CHECK = '\u2713';
+		final char COPYRIGHT = '\u00A9';
+		final char DOLLAR = '\u0024';
+		//final char CHERRY = '\uD83C\uDF52'; //\u1F352
+		//final char TURD =  '\uD83D\uDCA9'; //\u1F4A9
+		//final char CLOVER = '\uD83C\uDF40'; //\u1F340
+
+		System.out.println(SMILEY_FACE);
+		System.out.println(HEART);
+		System.out.println(DOLLAR);
+		System.out.println(SEVEN);
+		System.out.println(THREE);
+		System.out.println(FROWN);
+		System.out.println(DIAMOND);
+		System.out.println(EX);
+		System.out.println(CHECK);
+		System.out.println(COPYRIGHT);
+	}
+}
+
