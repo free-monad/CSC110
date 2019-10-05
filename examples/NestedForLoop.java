d9a212a4a130011f19374abc6ab0bee8f6196ecd
Matthew Conroy <matthewscottconroy@gmail.com>
2019-10-05 16:42:37 -0400

    Fixed nestedloop example

--- examples/NestedForLoop.java
+++ examples/NestedForLoop.java
@@ -7,5 +7,16 @@ public class NestedForLoop{
        }
        System.out.print("\n");
      }
-        
+		 
+
+		System.out.println("\n\n\n");
+
+		for(int i = 0; i < 7; i++){
+			for(int j = 1; j < i; j++){
+				System.out.print("#");
+			}
+			System.out.print("\n");
+		}
+       
   }
+}
