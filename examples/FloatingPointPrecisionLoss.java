39219372628f86487b5f60b0c6d67ed4c8b13acc
Matthew Conroy <matthewscottconroy@gmail.com>
2019-10-05 05:24:33 -0400

    added a loss of precision example

---
+++ examples/FloatingPointPrecisionLoss.java
@@ -0,0 +1,12 @@
+public class FloatingPointPrecisionLoss {
+	public static void main(String [] args){
+		double d = 0.2;
+		float f = 0.2f;
+
+		System.out.println(d);
+		System.out.println(f);
+
+		System.out.println(d*d*d*d*d*d*d);
+		System.out.println(f*f*f*f*f*f*f);
+	}
+}
