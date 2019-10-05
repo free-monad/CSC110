1222fa32ce9ce74d4cb4fba268ab666f8ec1981e
free-monad <54972647+free-monad@users.noreply.github.com>
2019-10-05 16:35:04 -0400

    Create NestedForLoop.java

---
+++ examples/NestedForLoop.java
@@ -0,0 +1,11 @@
+public class NestedForLoop{
+  public static void main (String [] args){
+    
+    for(int i = 0; i < 5; i++){
+      for(int j = 0; j < 3; j++){
+        System.out.print("*");
+       }
+       System.out.print("\n");
+     }
+        
+  }
