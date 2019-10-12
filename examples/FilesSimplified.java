08b2ef07db4104ca7874f026d549e7a05e83e57e
free-monad <54972647+free-monad@users.noreply.github.com>
2019-10-12 07:46:54 -0400

    Update FilesSimplified.java

--- examples/FilesSimplified.java
+++ examples/FilesSimplified.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
 
-public class FilesSimplified{
+public class FilesSimplified throws IOException{
   public static void main(String [] args){
     Scanner fileIn = new Scanner(new File("example.txt"));
     
