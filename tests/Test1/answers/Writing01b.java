396a6455a5bad6aeebc087d0151b201bb1db3b97
free-monad <54972647+free-monad@users.noreply.github.com>
2019-10-19 19:08:55 -0400

    Update Writing01b.java

--- tests/Test1/answers/Writing01b.java
+++ tests/Test1/answers/Writing01b.java
@@ -1,3 +1,9 @@
+/*
+Write a program that stores two numbers each in their own variable. Print each number to the
+screen. Next swap the numbers so that the first variable has the number from the second variable and
+the second variable has the number from the first variable. Print them to the screen again after the
+swap.
+*/
 public class Writing01b{
 	public static void main(String [] args){
 		int x = 30;
