8a4435c8263e01392aefac5ce7a7aaa09f6d9638
free-monad <54972647+free-monad@users.noreply.github.com>
2019-10-19 19:16:08 -0400

    Update Writing10.java

--- tests/Test1/answers/Writing10.java
+++ tests/Test1/answers/Writing10.java
@@ -1,3 +1,7 @@
+/*
+Write a program that takes input from the user, converts all of the letters to lowercase, and
+removes any whitespace from the edges.
+*/
 import java.util.Scanner;
 
 public class Writing10{
