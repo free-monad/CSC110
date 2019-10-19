eb7584e3b3f28c5cd47d3b9acd83db19cb0b60a3
free-monad <54972647+free-monad@users.noreply.github.com>
2019-10-19 19:12:11 -0400

    Update Writing05.java

--- tests/Test1/answers/Writing05.java
+++ tests/Test1/answers/Writing05.java
@@ -1,3 +1,7 @@
+/*
+Write a program that prints out a random number between 0 and 1 [0,1) where 1 is non-
+inclusive.
+*/
 public class Writing05{
 	public static void main(String [] args){
 		System.out.println(Math.random());	
